7ad838e193e10391f1b8c2aa327f3e769c5ae9d4
Xarlos89 <57622136+Xarlos89@users.noreply.github.com>
2023-10-16 16:26:22 +0200

    Exploring Logging - NOT DONE YET

--- app/__main__.py
+++ app/__main__.py
@@ -3,18 +3,32 @@ __main__.py entrypoint for the bot.
 """
 import os
 import discord
-from logging import log
 
 from Eos.app.db.database import DB
+from logger import log
 
 
 client = discord.Client(intents=discord.Intents.all())
 
+
+@client.event
+async def before_identify_hook(shard_id, initial):
+    if initial:
+        log.info('Connecting to DB')
+        # db = DB(os.getenv('RETOOL_DB'))
+        log.info('Connection Success')
+
 @client.event
 async def on_ready():
-    db = DB(os.getenv('RETOOL_DB'))
-    db.sync(client)
+    client.THROW_EXCEPTION
+    # db.sync(client)
     log.info(f'We have logged in as {client.user}')
+
+@client.event
+async def on_error(event, *args, **kwargs):
+    log.critical(event)
+
+
 
 
 if __name__ == "__main__":
