c932e9f90d32d9b81e3d564ac6e5780491248dd2
Xarlos89 <57622136+Xarlos89@users.noreply.github.com>
2024-10-06 21:06:41 +0200

    Update DB for logging channels.

--- src/db/init.sql
+++ src/db/init.sql
@@ -12,11 +12,17 @@ CREATE TABLE IF NOT EXISTS settings (
     name VARCHAR(255) NOT NULL,
     value VARCHAR(255)
 );
+
 INSERT INTO settings (name, value)
 VALUES
-    ('points', 'True'),
-    ('logging', 'True'),
-    ('antispam', 'True');
+    ('Verification Log', '0000000000000000000'),
+    ('Join Log', '0000000000000000000'),
+    ('Chat Log', '0000000000000000000'),
+    ('User Log', '0000000000000000000'),
+    ('Mod Log', '0000000000000000000'),
+    ('Server Log', '0000000000000000000'),
+    ('Error Log', '0000000000000000000');
+
 
 
 -- Create users table
