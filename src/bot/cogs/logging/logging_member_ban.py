d5af2de8e78948e88a81afe8b659ae025772891a
Xarlos89 <57622136+Xarlos89@users.noreply.github.com>
2024-12-08 16:23:25 +0100

    Logging - member bans

---
+++ src/bot/cogs/logging/logging_member_ban.py
@@ -0,0 +1,63 @@
+"""
+Logs when a member gets banned.
+"""
+import logging
+from datetime import datetime
+import discord
+from discord.ext import commands
+
+
+logger = logging.getLogger(__name__)
+
+
+def embed_ban(some_member, audit_log_entry):
+    """
+    Embedding for user ban alerts.
+    """
+    embed = discord.Embed(
+        title=""
+        , description=f"{some_member} was banned by: {audit_log_entry.user}. Reason: {audit_log_entry.reason}"
+        , color=discord.Color.red()
+        , timestamp=datetime.utcnow()
+    )
+    return embed
+
+
+class LoggingBans(commands.Cog):
+    """
+    Simple listener to on_member_remove
+    then checks the audit log for exact details
+    """
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_member_remove(self, member):
+        """
+        First we don't log bans for unapproved people.
+        then we grab the guild, and from there read the last entry in the audit log.
+        """
+        # TODO: Verification
+        # This needs to be toggle-able once verification is added to settings.
+        if "Needs Approval" in [role.name for role in member.roles]:
+            return
+
+        audit_log = [entry async for entry in member.guild.audit_logs(limit=1)][0]
+
+        channel = self.bot.api.get_one_setting("2") # Join_log
+        if channel[0]["status"] == "ok":
+            logs_channel = await self.bot.fetch_channel(channel[0]["settings"][2])
+
+
+            if str(audit_log.action) == "AuditLogAction.ban":
+                if audit_log.target == member:
+                    embed = embed_ban(member, audit_log)
+
+                    await logs_channel.send(embed=embed)
+                    return
+
+
+async def setup(bot: commands.Bot) -> None:
+    """boink"""
+    await bot.add_cog(LoggingBans(bot))
