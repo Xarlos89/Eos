a45c76fbf0e1e31998f1bdb77ba58ad3a86443b9
Xarlos89 <57622136+Xarlos89@users.noreply.github.com>
2024-12-21 12:19:29 +0100

    Admin emergency

--- src/bot/cogs/moderation/admin_emergencey.py
+++ src/bot/cogs/moderation/admin_emergencey.py
@@ -1,1 +1,34 @@
-# TODO
+# pylint: skip-file
+# import discord
+# from discord import default_permissions
+
+from discord.ext import commands
+
+logger = logging.getLogger(__name__)
+
+
+class admin_emergencey(commands.Cog, command_attrs=dict(hidden=True)):
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    @commands.command(name="lockdown")
+    @commands.has_permissions(manage_channels=True)
+    async def lockdown(self, ctx):
+        logger.warning(f"{ctx.guild.name} is going into lockdown mode!")
+        for channel in ctx.guild.text_channels:
+            await channel.set_permissions(ctx.guild.default_role,send_messages=False)
+            await channel.send(f"***{channel.name} is now in lockdown.***")
+
+
+    @commands.command(name="unlock")
+    @commands.has_permissions(manage_channels=True)
+    async def unlock(self, ctx):
+        logger.warning(f"{ctx.guild.name} is coming out of lockdown mode!")
+        for channel in ctx.guild.text_channels:
+            await channel.set_permissions(ctx.guild.default_role, send_messages=None)
+            await channel.send(f"***{channel.name} has been unlocked.***")
+
+
+def setup(bot):
+    bot.add_cog(admin_emergencey(bot))
