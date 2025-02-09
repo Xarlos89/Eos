89a8c74de57b72cd79182638fba46ef5eb4552e8
Xarlos <57622136+Xarlos89@users.noreply.github.com>
2025-02-09 15:18:14 +0100

    ban logs now go to whatever is set in the "mod log"

--- src/bot/cogs/logging/logging_member_ban.py
+++ src/bot/cogs/logging/logging_member_ban.py
@@ -6,7 +6,6 @@ from datetime import datetime
 import discord
 from discord.ext import commands
 
-
 logger = logging.getLogger(__name__)
 
 
@@ -31,6 +30,8 @@ class LoggingBans(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+        self.verification_role = self.bot.api.get_one_role('6')[0]['roles'][2]  # Verification role ID
+        self.mod_log = self.bot.api.get_one_log_setting("5")  # mod_log
 
     @commands.Cog.listener()
     async def on_member_remove(self, member):
@@ -38,18 +39,16 @@ class LoggingBans(commands.Cog):
         First we don't log bans for unapproved people.
         then we grab the guild, and from there read the last entry in the audit log.
         """
-        verification_role = self.bot.api.get_one_role('6')[0]['roles'][2] # Verification role ID
-        if verification_role in [role.id for role in member.roles]:
+        if self.verification_role in [role.id for role in member.roles]:
             return
 
         audit_log = [entry async for entry in member.guild.audit_logs(limit=1)][0]
 
-        channel = self.bot.api.get_one_log_setting("2") # Join_log
-        if channel[0]["status"] == "ok":
-            if channel[0]["logging"][2] == "0":
-                logger.debug(f"log was triggered, but logging is disabled. API: {channel}")
+        if self.mod_log[0]["status"] == "ok":
+            if self.mod_log[0]["logging"][2] == "0":
+                logger.debug(f"log was triggered, but logging is disabled. API: {self.join_log}")
                 return
-            logs_channel = await self.bot.fetch_channel(channel[0]["logging"][2])
+            logs_channel = await self.bot.fetch_channel(self.mod_log[0]["logging"][2])
 
             if str(audit_log.action) == "AuditLogAction.ban":
                 if audit_log.target == member:
@@ -58,7 +57,7 @@ class LoggingBans(commands.Cog):
                     await logs_channel.send(embed=embed)
                     return
         else:
-            logger.critical(f"API error. API response not ok. -> {channel}")
+            logger.critical(f"API error. API response not ok. -> {self.mod_log}")
 
 
 async def setup(bot: commands.Bot) -> None:
