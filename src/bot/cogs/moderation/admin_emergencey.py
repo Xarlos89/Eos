082349538b9ffba8a89e1703582ad350e31a032f
Xarlos <57622136+Xarlos89@users.noreply.github.com>
2025-02-11 20:38:18 +0100

    make lockdown a slash command

--- src/bot/cogs/moderation/admin_emergencey.py
+++ src/bot/cogs/moderation/admin_emergencey.py
@@ -34,33 +34,39 @@ class AdminEmergencey(commands.Cog, command_attrs=dict(hidden=True)):
         self.bot = bot
 
     @commands.check(is_administrator)
-    @app_commands.command(name="locks down the server.")
     @commands.has_permissions(manage_channels=True)
-    async def lockdown(self, ctx):
-        logger.warning(f"{ctx.guild.name} is going into lockdown mode!")
-        for channel in ctx.guild.text_channels:
-            await channel.set_permissions(ctx.guild.default_role,send_messages=False)
+    @app_commands.command(name="lockdown", description="locks down the server.")
+    async def lockdown(self, interaction: discord.Interaction):
+        await interaction.response.defer()
+        logger.warning(f"{interaction.guild.name} is going into lockdown mode!")
+        for channel in interaction.guild.text_channels:
+            await channel.set_permissions(interaction.guild.default_role, send_messages=False)
             await channel.send(f"***{channel.name} is now in lockdown.***")
+        await interaction.followup.send(f"{interaction.guild.name} is now in lockdown. When ready, use **unlock**")
 
     @commands.check(is_administrator)
-    @app_commands.command(name="unlocks the server after a lockdown")
     @commands.has_permissions(manage_channels=True)
-    async def unlock(self, ctx):
-        logger.warning(f"{ctx.guild.name} is coming out of lockdown mode!")
-        for channel in ctx.guild.text_channels:
-            await channel.set_permissions(ctx.guild.default_role, send_messages=None)
+    @app_commands.command(name="unlock", description="unlocks the server after a lockdown")
+    async def unlock(self, interaction: discord.Interaction):
+        await interaction.response.defer()
+        logger.warning(f"{interaction.guild.name} is coming out of lockdown mode!")
+        for channel in interaction.guild.text_channels:
+            await channel.set_permissions(interaction.guild.default_role, send_messages=None)
             await channel.send(f"***{channel.name} has been unlocked.***")
+        await interaction.followup.send(f"{interaction.guild.name} is now unlocked.")
 
     @lockdown.error
     async def lockdown_error(self, ctx, error):
         if isinstance(error, commands.CheckFailure):
             await ctx.channel.send(embed=embed_info(
                 f"{ctx.author.mention}, you dont have permission to lock down the server. The staff has been notified."))
+
     @unlock.error
     async def unlock_error(self, ctx, error):
         if isinstance(error, commands.CheckFailure):
             await ctx.channel.send(embed=embed_info(
                 f"{ctx.author.mention}, you dont have permission to unlock the server. The staff has been notified."))
+
 
 async def setup(bot) -> None:
     """
