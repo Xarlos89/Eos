812d3437cdcf12afc1d0e86640f08af7a28a0f83
Xarlos <57622136+Xarlos89@users.noreply.github.com>
2025-02-11 20:00:58 +0100

    make ban_member a slash command

--- src/bot/cogs/moderation/admin_ban.py
+++ src/bot/cogs/moderation/admin_ban.py
@@ -5,10 +5,11 @@ import logging
 from datetime import datetime
 
 import discord
+from discord import app_commands
 from discord.ext import commands
 
-
 logger = logging.getLogger(__name__)
+
 
 def embed_info(message):
     """
@@ -30,6 +31,7 @@ async def is_moderator(ctx) -> bool:
     """
     return ctx.message.author.guild_permissions.ban_members
 
+
 class AdminBan(commands.Cog):
     """
     Command to ban a user. Takes in a name, and a reason.
@@ -39,9 +41,9 @@ class AdminBan(commands.Cog):
         self.bot = bot
 
     @commands.check(is_moderator)
-    @commands.command(description="Ban a user.")
     @commands.has_permissions(ban_members=True)
-    async def ban_member(self, ctx, target: discord.Member, reason):
+    @app_commands.command(description="Ban a user.")
+    async def ban_member(self, interaction: discord.interaction, target: discord.Member, reason: str):
         """
         Take in a user mention, and a string reason.
         """
@@ -50,21 +52,23 @@ class AdminBan(commands.Cog):
             if not target.guild_permissions.administrator:
                 # Message the user, informing them of their fate
                 # TODO: Guild specific settings like the contact email
+                await interaction.response.defer()
                 await target.send(
-                    f"## You were banned by {ctx.author.name}.\n"
+                    f"## You were banned by {interaction.user.name}.\n"
                     f"**Reason:** {reason}\n"
                     "\nIf you wish to appeal this ban,"
                     " contact PracticalPythonStaff@gmail.com"
                 )
                 # Then we do the ban
-                await target.ban(reason=f"{ctx.author.name} - {reason}")
-                logger.info("{%s} banned {%s}. Reason: {%s}", ctx.author.name, target.name, reason)
+                await target.ban(reason=f"{interaction.user.name} - {reason}")
+                logger.info("{%s} banned {%s}. Reason: {%s}", interaction.user.name, target.name, reason)
                 # Then we publicly announce what happened.
-                await ctx.channel.send(embed=embed_info(f"**{ctx.author.name}** banned **{target.name}**" f"\n**Reason:** {reason}"))
+                await interaction.followup.send(
+                    embed=embed_info(f"**{interaction.user.name}** banned **{target.name}**" f"\n**Reason:** {reason}"))
             else:
-                await ctx.channel.send(embed=embed_info("You can't ban an Admin."))
+                await interaction.channel.send(embed=embed_info("You can't ban an Admin."))
         else:
-            await ctx.channel.send(embed=embed_info("You cant ban a bot."))
+            await interaction.channel.send(embed=embed_info("You cant ban a bot."))
 
     @ban_member.error
     async def ban_error(self, ctx, error):
@@ -75,6 +79,7 @@ class AdminBan(commands.Cog):
         if isinstance(error, commands.CheckFailure):
             await ctx.channel.send(embed=embed_info(
                 f"{ctx.author.mention}, you dont have permission to ban users. The staff has been notified."))
+
 
 async def setup(bot) -> None:
     """
