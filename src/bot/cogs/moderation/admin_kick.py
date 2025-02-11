6353a07ea97cb33a7fd28a822a0e0cdb2cd1ef54
Xarlos <57622136+Xarlos89@users.noreply.github.com>
2025-02-11 13:24:58 +0100

    make kick_member a slash command

--- src/bot/cogs/moderation/admin_kick.py
+++ src/bot/cogs/moderation/admin_kick.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 import discord
 from discord.ext import commands
-
+from discord import app_commands
 
 logger = logging.getLogger(__name__)
 
@@ -23,12 +23,14 @@ def embed_info(message):
     )
     return embed
 
+
 async def is_moderator(ctx) -> bool:
     """
     Check if the context user has moderator permissions
     https://discordpy.readthedocs.io/en/stable/api.html?highlight=guild_permissions#discord.Permissions.kick_members
     """
     return ctx.message.author.guild_permissions.kick_members
+
 
 class AdminKick(commands.Cog):
     """
@@ -37,34 +39,37 @@ class AdminKick(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+
     @commands.check(is_moderator)
-    @commands.command(description="Kick a user.")
+    @app_commands.command(description="Kick a user.")
     @commands.has_permissions(kick_members=True)
-    async def kick_member(self, ctx, target: discord.Member, reason):
+    async def kick_member(self, interaction: discord.Interaction, target: discord.Member, reason: str):
         """
         Take in a user mention, and a string reason.
         """
         # Cant kick bots or admins.
         if not target.bot:
+            await interaction.response.defer()
             if not target.guild_permissions.administrator:
                 # Message the user, informing them of their fate
                 # TODO: Guild specific settings like the contact email
                 await target.send(
-                    f"## You were kicked by {ctx.author.name}.\n"
+                    f"## You were kicked by {interaction.user.name}.\n"
                     f"**Reason:** {reason}\n"
                     "\nIf you wish to appeal this kick,"
                     " contact PracticalPythonStaff@gmail.com"
                 )
                 # Then we do the kick
-                await target.kick(reason=f"{ctx.author.name} - {reason}")
-                logger.info("{%s} kicked {%s}. Reason: {%s}", ctx.author.name, target.name, reason)
+                await target.kick(reason=f"{interaction.user.name} - {reason}")
+                logger.info("{%s} kicked {%s}. Reason: {%s}", interaction.user.name, target.name, reason)
                 # Then we publicly announce what happened.
-                await ctx.channel.send(embed=embed_info(f"**{ctx.author.name}** kicked **{target.name}**" f"\n**Reason:** {reason}"))
+                await interaction.channel.send(
+                    embed=embed_info(f"**{interaction.user.name}** kicked **{target.name}**" f"\n**Reason:** {reason}"))
 
             else:
-                await ctx.channel.send(embed=embed_info("You can't kick an Admin."))
+                await interaction.channel.send(embed=embed_info("You can't kick an Admin."))
         else:
-            await ctx.channel.send(embed=embed_info("You cant kick a bot."))
+            await interaction.channel.send(embed=embed_info("You cant kick a bot."))
 
     @kick_member.error
     async def kick_error(self, ctx, error):
