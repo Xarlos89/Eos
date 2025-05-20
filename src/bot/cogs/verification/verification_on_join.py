038f21f1fefc791cec9d622577412ca24468fa24
Xarlos89 <57622136+Xarlos89@users.noreply.github.com>
2025-05-20 17:25:40 +0200

    Fix incorrect guild attribute reference in verification check
        Updated the guild attribute reference from `message.guild.id` to `message.author.guild.id` to ensure proper checks in the verification logic. This resolves potential issues with handling events in the master guild.

--- src/bot/cogs/verification/verification_on_join.py
+++ src/bot/cogs/verification/verification_on_join.py
@@ -58,8 +58,9 @@ class LoggingVerification(commands.Cog):
     @commands.Cog.listener()
     async def on_member_join(self, member: discord.Member):
         guild = member.guild
+        guild_id = member.guild.id
 
-        if member.guild.id != int(os.getenv("MASTER_GUILD")):
+        if guild_id != int(os.getenv("MASTER_GUILD")):
             logger.warning("on_member_join fired, but not in master guild. Ignoring event.")
             return
 
@@ -69,34 +70,34 @@ class LoggingVerification(commands.Cog):
         await self.send_welcome_message(guild, member)
         await self.kick_if_not_verified(member, 3600, verification_log)
 
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        """
-        Keep verification clean again
-        """
-        if message.author.guild.id != int(os.getenv("MASTER_GUILD")):
-            logger.warning("on_message in verification fired, but not in master guild. Ignoring event.")
-            return
-
-        if message.channel.id == int(self.verification_channel):
-            if not message.author.bot:
-                if f"{os.getenv('PREFIX')}verify" == message.content:  # keep it exact
-                    # user is doing it right, and the verification_dropdown is triggered
-                    logger.debug(f"{message.author.name} started verification.")
-                    await sleep(3)
-                    await message.delete()  # cleanup correct verification calls
-                    return
-                else:  # this covers any other message in the channel.
-                    await message.delete()  # delete the user's incorrect message
-                    bot_message = await message.channel.send(
-                        f"You need to use the **{os.getenv('PREFIX')}verify** command.")
-
-                    logs_channel = await self.bot.fetch_channel(self.verification_log)
-                    await logs_channel.send(
-                        f"{message.author} is failing at life in {self.bot.get_channel(self.verification_channel)}")
-
-                    await sleep(8)
-                    bot_message.delete()  # remove the message to correct people after 8? seconds
+    # @commands.Cog.listener()
+    # async def on_message(self, message):
+    #     """
+    #     Keep verification clean again
+    #     """
+    #     if message.author.guild.id != int(os.getenv("MASTER_GUILD")):
+    #         logger.warning("on_message in verification fired, but not in master guild. Ignoring event.")
+    #         return
+    #
+    #     if message.channel.id == int(self.verification_channel):
+    #         if not message.author.bot:
+    #             if f"{os.getenv('PREFIX')}verify" == message.content:  # keep it exact
+    #                 # user is doing it right, and the verification_dropdown is triggered
+    #                 logger.debug(f"{message.author.name} started verification.")
+    #                 await sleep(3)
+    #                 await message.delete()  # cleanup correct verification calls
+    #                 return
+    #             else:  # this covers any other message in the channel.
+    #                 await message.delete()  # delete the user's incorrect message
+    #                 bot_message = await message.channel.send(
+    #                     f"You need to use the **{os.getenv('PREFIX')}verify** command.")
+    #
+    #                 logs_channel = await self.bot.fetch_channel(self.verification_log)
+    #                 await logs_channel.send(
+    #                     f"{message.author} is failing at life in {self.bot.get_channel(self.verification_channel)}")
+    #
+    #                 await sleep(8)
+    #                 bot_message.delete()  # remove the message to correct people after 8? seconds
 
 
 async def setup(bot: commands.Bot) -> None:
