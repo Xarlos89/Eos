bfb43eb0fe4e4127b6e46c1724f41ffd132811b9
Xarlos <57622136+Xarlos89@users.noreply.github.com>
2025-08-17 14:37:18 +0200

    fix automoderation api call

--- src/bot/cogs/moderation/admin_automod_spam_messages.py
+++ src/bot/cogs/moderation/admin_automod_spam_messages.py
@@ -170,14 +170,14 @@ class ModerationSpamMessages(commands.Cog):
             message (discord.Message): The triggering message object.
             record (dict): The user's message record.
         """
-        naughty_role = self.bot.api.get_one_role("7")
-        verified_role = self.bot.api.get_one_role("6")
+        naughty_role = self.bot.api.get_one_role("7")[0]["roles"][2]
+        verified_role = self.bot.api.get_one_role("6")[0]["roles"][2]
         quarantine_channel = self.bot.api.get_one_setting("2")[0]["setting"][2]
         quarantine_channel = await self.bot.fetch_channel(quarantine_channel)
         thirty_seconds = datetime.now().astimezone() + timedelta(seconds=30)
         await message.author.timeout(thirty_seconds, reason="Sending the same message multiple times.")
-        await message.author.remove_roles(verified_role[0]["roles"][2])
-        await message.author.add_roles(naughty_role[0]["roles"][2])
+        await message.author.remove_roles(verified_role)
+        await message.author.add_roles(naughty_role)
 
         await quarantine_channel.send(
             embed=embed_spammer(message.author, message.content, record["messages"][-1]["file_url"])
