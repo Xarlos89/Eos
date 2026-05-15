9c5cbdb9bae92828b49b24890968404fbd82149e
Kritarth <kritarthvarma2135@gmail.com>
2026-05-15 23:28:57 +0530

    Added migration commands

--- src/db/migrations.sql
+++ src/db/migrations.sql
@@ -2,4 +2,19 @@ BEGIN;
 
     -- Put all your migrations commands here
 
+    INSERT INTO serversettings (name, value)
+    VALUES ('Server Announcement Channel', '0');
+
+    INSERT INTO roles (name, value)
+    VALUES ('Yapper', '0');
+
+    ALTER TABLE users ADD COLUMN monthly_points int NOT NULL DEFAULT 0;
+
+    CREATE TABLE IF NOT EXISTS parameters (
+        parameter_name VARCHAR(255) NOT NULL,
+        parameter_value VARCHAR(255)
+    );
+    INSERT INTO parameters (parameter_name, parameter_value)
+    VALUES ('monthly_yapper', '0');
+
 COMMIT;
