7752c5ea3c3692baebd67b9cabeef430c5106837
Kritarth <kritarthvarma2135@gmail.com>
2026-05-17 18:29:08 +0530

    Added EXISTS guard in all migration statements

--- src/db/migrations.sql
+++ src/db/migrations.sql
@@ -1,20 +1,34 @@
 BEGIN;
 
-    -- Put all your migrations commands here
+    -- Put all your migrations commands here.
+    -- It is HIGHLY recommended to use EXISTS guards to not
+    -- accidentally apply migrations more than once.
 
     INSERT INTO serversettings (name, value)
-    VALUES ('Server Announcement Channel', '0');
+    SELECT 'Server Announcement Channel', '0'
+    WHERE NOT EXISTS (
+        SELECT * FROM serversettings
+        WHERE name='Server Announcement Channel'
+    );
 
     INSERT INTO roles (name, value)
-    VALUES ('Yapper', '0');
+    SELECT 'Yapper', '0'
+    WHERE NOT EXISTS (
+        SELECT * FROM roles
+        WHERE name='Yapper'
+    );
 
-    ALTER TABLE users ADD COLUMN monthly_points int NOT NULL DEFAULT 0;
+    ALTER TABLE users ADD COLUMN IF NOT EXISTS monthly_points int NOT NULL DEFAULT 0;
 
     CREATE TABLE IF NOT EXISTS parameters (
         parameter_name VARCHAR(255) NOT NULL,
         parameter_value VARCHAR(255)
     );
     INSERT INTO parameters (parameter_name, parameter_value)
-    VALUES ('monthly_yapper', '0');
+    SELECT 'monthly_yapper', '0'
+    WHERE NOT EXISTS (
+        SELECT * FROM parameters
+        WHERE parameter_name='monthly_yapper'
+    );
 
 COMMIT;
