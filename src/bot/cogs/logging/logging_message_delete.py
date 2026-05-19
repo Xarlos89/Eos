21826c135e2073be994f5512c33e0047be5862b9
Minus <78946158+Minus-hates-school@users.noreply.github.com>
2026-05-19 00:01:54 -0400

    Removed [0]

--- src/bot/cogs/logging/logging_message_delete.py
+++ src/bot/cogs/logging/logging_message_delete.py
@@ -49,10 +49,22 @@ class LoggingMessageDelete(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self.staff_channel = self.bot.api.get_one_setting('3')#['setting'][2]  # Staff Channel ID
-        logger.debug(f"LoggingMessageDelete cog initialized. Staff channel ID: {self.staff_channel}")
+        # 'setting': [3, 'Staff Channel', '1506111576946511923'], 'status': 'ok'}
+        setting = self.bot.api.get_one_setting('3')
+        if setting['status'] != 'ok':
+            logger.error(f"API error. API response not ok. -> {setting}")
+            return 
+        else:
+            self.staff_channel = setting['setting'][2]
+            logger.debug(f"LoggingMessageDelete cog initialized. Staff channel ID: {self.staff_channel}")
+            
+        #{'logging': [3, 'Chat Log', '1394473942537994363'], 'status': 'ok'}
         self.chat_log = self.bot.api.get_one_log_setting("3")  # chat_log
-        logger.debug(f"LoggingMessageDelete cog initialized. Chat log settings: {self.chat_log}")
+        if self.chat_log['status'] != 'ok':
+            logger.error(f"API error. API response not ok. -> {self.chat_log}")
+            return
+        else:
+            logger.debug(f"LoggingMessageDelete cog initialized. Chat log settings: {self.chat_log}")
 
     @commands.Cog.listener()
     async def on_message_delete(self, message):
@@ -69,11 +81,11 @@ class LoggingMessageDelete(commands.Cog):
             return
 
         audit_log = [entry async for entry in message.guild.audit_logs(limit=1)][0]
-        if self.chat_log[0]["status"] == "ok":
-            if self.chat_log[0]["logging"][2] == "0":
+        if self.chat_log["status"] == "ok":
+            if self.chat_log["logging"][2] == "0":
                 logger.debug(f"log was triggered, but logging is disabled. API: {self.chat_log}")
                 return
-            logs_channel = await self.bot.fetch_channel(self.chat_log[0]["logging"][2])
+            logs_channel = await self.bot.fetch_channel(self.chat_log["logging"][2])
 
             if str(audit_log.action) == 'AuditLogAction.message_delete':
                 # Then a moderator deleted a message.
