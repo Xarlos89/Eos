f0d3634d2eaff59172497cef05d4b6ab49b1d501
Minus <78946158+Minus-hates-school@users.noreply.github.com>
2026-05-21 15:58:15 -0400

    gc

--- src/bot/cogs/features/ticket.py
+++ src/bot/cogs/features/ticket.py
@@ -153,8 +153,8 @@ class TicketManager(commands.Cog):
                 close = self.bot.api.update_ticket_status(thread.id, "closed") # type: ignore
                 if close is None or close['status'] != 'ok':
                     logger.warning(f"Failed to update ticket status to closed for channel {thread.id}.")
-                else:
                     await interaction.response.send_message("This thread is unknown to the DB", ephemeral=True)
+                else:
                     try:
                         await thread.send(f"This ticket has been closed by {interaction.user.mention}.")
                         await thread.edit(archived=True, locked=True) # type: ignore
