9be5363f1fc3141d80958391541e60ce394175ea
Minus <78946158+Minus-hates-school@users.noreply.github.com>
2026-05-21 08:53:33 -0400

    ReWorked Selector

--- src/bot/cogs/features/ticket.py
+++ src/bot/cogs/features/ticket.py
@@ -9,67 +9,13 @@ from discord.ext import commands
 
 logger = logging.getLogger(__name__)
 
-class TicketReasonModal(discord.ui.Modal, title="Create a Ticket"):
+class TicketReasonModal(discord.ui.Modal, title="Create Ticket"):
     """
     This modal appears when the user selects a ticket type.
     It allows them to provide a reason or description for the ticket.
     """
-    description = discord.ui.TextInput(
-        label="Please describe the issue or reason for the ticket. ", 
-        style=discord.TextStyle.paragraph, 
-        required=True, max_length=1000
-        )
-
-    def __init__(self, bot, ticket_type: str):
-        super().__init__()
-        self.ticket_type = ticket_type
-        self.bot = bot
-
-    async def on_submit(self, interaction: discord.Interaction):
-        guild = interaction.guild
-        channel = self.bot.api.get_one_setting('5')
-        
-        if channel is None or channel['setting'][2] == "0":
-            logger.warning("Ticket Channel not set in db. Cannot create ticket.")
-            await interaction.response.send_message("Sorry, the ticket system is not set up yet. Please contact the staff directly.", ephemeral=True)
-            return
-        else:
-            channel = guild.get_channel(int(channel['setting'][2])) # type: ignore
-
-        
-
-        thread_name = f"{self.ticket_type}-{interaction.user.name}".lower().replace(" ", "-")
-
-        
-        thread = await channel.create_thread( # type: ignore
-            name=thread_name,
-            type=discord.ChannelType.private_thread,
-            invitable=False
-            ) 
-        await thread.add_user(interaction.user) # type: ignore
-        
-        await thread.send(
-            f'<@&{self.staff_role}> -- {interaction.user.mention} has created a ticket\n'
-            f'Type: {self.ticket_type}\n'
-            f'{self.description.value}'
-            f'\n\n please provide any additional information here and our staff will assist you as soon as possible.'
-        )
-        
-        self.bot.db.create_ticket(interaction.user.id, thread.id, self.ticket_type, self.description.value)
-        
-        await interaction.response.send_message(f"Your ticket has been created! {thread.jump_url}", ephemeral=True)
-        
-
-class TicketDropdown(discord.ui.Select):
-    """
-    This is the dropdown that appears when the user clicks 
-    the button to create a ticket. It allows them to select the 
-    type of ticket they want to create.
-    """
-    def __init__(self, bot):
-        self.bot = bot
-        
-        options = [
+    
+    options = [
             discord.SelectOption(
                 label="Moderation",
                 description="Report users or moderation issues"
@@ -87,25 +33,64 @@ class TicketDropdown(discord.ui.Select):
                 description="Request something from staff"
             )
         ]
-        
-        super().__init__(
-            placeholder="Please select a ticket type",
-            min_values=1,
-            max_values=1,
-            options=options)
+    selected_option = discord.ui.Select(
+        custom_id="ticket_type",
+        options=options,
+        placeholder="Select the type of ticket you want to create",
+        )
     
-    async def callback(self, interaction: discord.Interaction):
-        selected = self.values[0]
-        await interaction.response.send_modal(TicketReasonModal(self.bot, selected))
+    description = discord.ui.TextInput(
+        label="Please describe the issue or reason for the ticket. ", 
+        style=discord.TextStyle.paragraph, 
+        required=True, max_length=1000
+        )
+
+
+    
+    def __init__(self, bot):
+        super().__init__()
+        self.bot = bot
+
+    async def on_submit(self, interaction: discord.Interaction):
+        guild = interaction.guild
+        channel = self.bot.api.get_one_setting('5')
+        
+        if channel is None or channel['setting'][2] == "0":
+            logger.warning("Ticket Channel not set in db. Cannot create ticket.")
+            await interaction.response.send_message("Sorry, the ticket system is not set up yet. Please contact the staff directly.", ephemeral=True)
+            return
+        else:
+            channel = guild.get_channel(int(channel['setting'][2])) # type: ignore
+
+        option = self.selected_option.values[0] if self.selected_option.values else "None provided"
+
+        thread_name = f"{option}-{interaction.user.name}".lower().replace(" ", "-")
 
         
-
+        thread = await channel.create_thread( # type: ignore
+            name=thread_name,
+            type=discord.ChannelType.private_thread,
+            invitable=False 
+            ) 
+        await thread.add_user(interaction.user) # type: ignore
+        
+        await thread.send(
+            f'<@&{self.staff_role}> -- {interaction.user.mention} has created a ticket\n'
+            f'Type: {option}\n'
+            f'{self.description.value}'
+            f'\n\n please provide any additional information here and our staff will assist you as soon as possible.'
+        )
+        
+        self.bot.db.create_ticket(interaction.user.id, thread.id, option, self.description.value)
+        
+        await interaction.response.send_message(f"Your ticket has been created! {thread.jump_url}", ephemeral=True)
+        
 
 class TicketView(discord.ui.View):
     def __init__(self, bot):
         super().__init__(timeout=None)
         self.bot = bot
-        self.add_item(TicketDropdown(bot))
+        self.add_item(TicketReasonModal(bot))
 
     
 
