78e53e104e5423374eb0bb768d875cc27120da75
Kritarth <kritarthvarma2135@gmail.com>
2026-05-29 13:55:27 +0530

    Added the migration commands.

--- src/db/migrations.sql
+++ src/db/migrations.sql
@@ -1,5 +1,23 @@
 BEGIN;
 
-    -- Put all your migrations commands here
+    -- Put all your migrations commands here.
+        -- It is HIGHLY recommended to use EXISTS guards to not
+    -- accidentally apply migrations more than once.
+
+    INSERT INTO serversettings (name, value)
+    SELECT 'Ticket Channel', '0'
+    WHERE NOT EXISTS (
+        SELECT * FROM serversettings
+        WHERE name='Ticket Channel'
+    );
+
+    CREATE TABLE IF NOT EXISTS tickets (
+        id SERIAL PRIMARY KEY,
+        thread_id BIGINT NOT NULL UNIQUE,
+        channel_id BIGINT NOT NULL,
+        creator_id BIGINT NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        status VARCHAR(50) DEFAULT 'open'
+    );
 
 COMMIT;
