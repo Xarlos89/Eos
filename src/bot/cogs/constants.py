71a7549c05f80f56f28fc2022dbb372d5451fece
Krish <101330162+Jiggly-Balls@users.noreply.github.com>
2026-08-22 22:16:41 +0530

    Change WEBPAGE_CHOICES from list to tuple

--- src/bot/cogs/constants.py
+++ src/bot/cogs/constants.py
@@ -1,6 +1,7 @@
 from discord.app_commands import Choice
 
-WEBPAGE_CHOICES: tuple[Choice[str], ...] = [
+
+WEBPAGE_CHOICES: tuple[Choice[str], ...] = (
     Choice(name="Home", value=""),
     Choice(name="Getting started", value="getting-started/"),
     Choice(name="Resources", value="resources/"),
@@ -9,7 +10,7 @@ WEBPAGE_CHOICES: tuple[Choice[str], ...] = [
     Choice(name="Code Adventure", value="code-adventure/"),
     Choice(name="Learning path", value="resources/learning-path/"),
     Choice(name="Codeblocks", value="rules/channels/#__tabbed_2_1"),
-]
+)
 
 LATEX_COLORS = {
     "emerald": "Emerald",
